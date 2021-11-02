0c3f3adb51d8cecc84a07dbffb5969ed6f59d144
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-02 14:47:20 +0700

    Enter

--- resources/views/Main.blade.php
+++ resources/views/Main.blade.php
@@ -1,6 +1,7 @@
-@include('Layout.navigation')
+@include('layoutPVZ.navbar')
    @extends('layoutPVZ.mainlayout')
    @section('Title',"Plants vs Zombie")
    @section('List')
     <h1> Plants vs Zombie Editor </h1>
+    <h1>Add new Zombie</h1>
    @endsection
