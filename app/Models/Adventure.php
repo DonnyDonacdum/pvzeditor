13c5da8af382dd7ccca424c2a00c28fb199960fd
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-02 15:28:47 +0700

    Enter

--- app/Models/Adventure.php
+++ app/Models/Adventure.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Adventure extends Model
 {
     use HasFactory;
-    
+    public static function allData(){
+        return self::$Adventure;
+    } 
 }
