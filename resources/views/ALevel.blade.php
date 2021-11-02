9f836efbac93bedb3ba6a391a15488249fa453e0
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-02 15:07:30 +0700

    Enter

--- resources/views/ALevel.blade.php
+++ resources/views/ALevel.blade.php
@@ -1,6 +1,15 @@
 @include('layoutPVZ.navbar')
    @extends('layoutPVZ.mainlayout')
-   @section('Title',"Plants vs Zombie")
+   @section('Title',"Adventure Level List")
    @section('List')
+    <div>
+        <h1>Adventure level List</h1>
+    </div>
+    <table>
     
+
+
+
+
+    </table>
    @endsection
