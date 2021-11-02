1b01eafd65fd507eae76d1fa67cd7983a91595b5
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-02 16:39:01 +0700

    Enter

--- resources/views/CreateLevel.blade.php
+++ resources/views/CreateLevel.blade.php
@@ -4,17 +4,17 @@
    @section('List')
    <form action="/action_page.php" method="post">
   <label for="fname">Level:</label><br>
-  <input type="text" id="fname" name="fname" required><br>
+  <input type="text" id="fname" name="lecel" required><br>
   <label for="lname">Background:</label><br>
-  <input type="text" id="lname" name="lname" required><br><br>
+  <input type="text" id="lname" name="back" required><br><br>
   <label for="fname">Gimmick:</label><br>
-  <input type="text" id="fname1" name="fname" required><br>
+  <input type="text" id="fname1" name="gim" required><br>
   <label for="lname">Flags:</label><br>
-  <input type="text" id="lname2" name="lname" required><br><br>
+  <input type="text" id="lname2" name="fleg" required><br><br>
   <label for="lname">Waves:</label><br>
-  <input type="text" id="lname2" name="lname" required><br><br>
+  <input type="text" id="lname2" name="wave" required><br><br>
   <label for="lname">description:</label><br>
-  <input type="text" id="lname3" name="lname" required><br><br>
+  <input type="text" id="lname3" name="descrip" required><br><br>
   <input type="submit" value="Submit">
 </form>
    @endsection
