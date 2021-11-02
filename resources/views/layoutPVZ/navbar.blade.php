94daacd41ebf3ec62a53c88d776acda6cf7b5d3f
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-02 14:54:02 +0700

    Enter

--- resources/views/layoutPVZ/navbar.blade.php
+++ resources/views/layoutPVZ/navbar.blade.php
@@ -1,14 +1,15 @@
-<nav class="navbar navbar-default">
-
-<div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+<nav class="navbar navbar-expand-sm bg-light">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link 3</a>
+      </li>
     </ul>
   </div>
 </nav>
