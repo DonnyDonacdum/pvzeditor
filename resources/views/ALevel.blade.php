9e29beb292404bdc05a541b2b4764e65bdea0d0d
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-02 15:18:29 +0700

    Enter

--- resources/views/ALevel.blade.php
+++ resources/views/ALevel.blade.php
@@ -6,10 +6,20 @@
         <h1>Adventure level List</h1>
     </div>
     <table>
-    
-
-
-
-
+    <tr>
+        <th>No</th>
+        <th>Level</th>
+        <th>Background</th>
+        <th>Gimmick</th>
+        <th>Flags</th>
+        <th>Wave counts</th>
+    </tr>
+    @php ($huh=0)
+    @foreach($adventure as $level)
+    <tr>
+        <td>{{$huh++}}</td>
+        <td>{{}}</td>
+    </tr>
+    @endforeach
     </table>
    @endsection
