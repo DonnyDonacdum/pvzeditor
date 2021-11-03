4c58cbbaaf7308c50d1d0b6b02a7aca7d55a3d2e
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-03 19:46:26 +0700

    Enter

--- app/Models/Adventures.php
+++ app/Models/Adventures.php
@@ -10,7 +10,7 @@ class Adventure extends Model
     use HasFactory;
     protected $table = 'adventure';
     protected $fillable = ['level','background','gimmick','flag','wave','description'];
-    public static function all(){
+    public static function allData(){
         return self::$adventure;
     } 
 }
