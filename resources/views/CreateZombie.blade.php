15f6dddcaaab492a9a60a6894cb41e031a642332
Donny The newspaper <Donnykurniawan1119@gmail.com>
2021-11-07 10:25:51 +0700

    Enter

---
+++ resources/views/CreateZombie.blade.php
@@ -0,0 +1,19 @@
+@include('layoutPVZ.navbar')
+   @extends('layoutPVZ.mainlayout')
+   @section('Title',"Create a Level")
+   @section('List')
+   <form action="{{route('Zombie.store')}}" method="post">
+      @csrf
+  <label for="fname">Level:</label><br>
+  <input type="text" id="fname" name="name" required><br>
+  <label for="lname">Background:</label><br>
+  <input type="text" id="lname" name="armor" required><br><br>
+  <label for="fname">Gimmick:</label><br>
+  <input type="text" id="fname1" name="special" required><br>
+  <label for="lname">Flags:</label><br>
+  <input type="text" id="lname2" name="sped" required><br><br>
+  <label for="lname">description:</label><br>
+  <input type="text" id="lname3" name="descript" required><br><br>
+  <input type="submit" value="Submit">
+</form>
+   @endsection
